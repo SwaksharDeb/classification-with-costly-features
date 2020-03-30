fab3a9930449af2fa767e0318f9419d2660bd9e6
SwaksharDeb <49334830+SwaksharDeb@users.noreply.github.com>
2020-03-31 01:56:26 +0600

    Add files via upload

--- tools/conv_wine.py
+++ tools/conv_wine.py
@@ -1,28 +1,33 @@
-# Instructions:
+# %% Instructions:
 # =============
 # 1.Download the wine dataset and Load the wine dataset
 # 2. Remove fist row and leading spaces
 # 3. Run this script
 
+# %% Import dependeces
 import pandas as pd
 import numpy as np
 from ipdb import set_trace
 from sklearn.utils import shuffle
 
+# %%
 SEED = 998822
 #---
 np.random.seed(SEED)
 
 COL_COUNT = '_count'
 
+# %% Import the wine dataset
 data = pd.read_csv("../data/raw/wine.csv")
 data[COL_COUNT] = 1  # adding a coloum named _count
 data = data.rename(columns={"Customer_Segment":"_label"}) # rename the label column
+data['_label'] = data['_label'] -1  # since class start from one
 data = data.sample(frac=1).reset_index(drop=True)
 
-TRAIN_SIZE = int(data.shape[0] * 0.35)  # 0.35%
+# %% train, validation, test split
+TRAIN_SIZE = int(data.shape[0] * 0.5)  # 0.35%
 VAL_SIZE   = int(data.shape[0] * 0.15)  # 0.15%
-TEST_SIZE  = int(data.shape[0] * 0.5)   # 0.5 %
+TEST_SIZE  = int(data.shape[0] * 0.35)   # 0.5 %
 
 data_train = data.iloc[0:TRAIN_SIZE]
 data_val   = data.iloc[TRAIN_SIZE:TRAIN_SIZE+VAL_SIZE]
@@ -34,11 +39,12 @@ print("Train len:", data_train.shape[0])
 print("Val len:  ", data_val.shape[0])
 print("Test len: ", data_test.shape[0])
 
+# %% Storing the training, validation and test data
 data_train.to_pickle("../data/wine-train")
 data_val.to_pickle("../data/wine-val")
 data_test.to_pickle("../data/wine-test")
 
-#--- prepare meta
+# %% prepare meta data and store it
 idx = data.columns[:-2]
 meta = pd.DataFrame(index=idx, dtype='float32')
 
